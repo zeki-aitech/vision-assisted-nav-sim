94a66b438ce5bebb3f07011175d8bf3f5a757b25
zeki-aitech <trungnh.aitech@gmail.com>
2026-02-15 01:36:47 +0000

    Remove deprecated tbot_description files and update .gitignore to include additional directories. Add build command to temp script.

---
+++ ros2_ws/src/tbot/tbot_navigation/tbot_nav_behavior/tbot_nav_behavior/perception_nav_node.py
@@ -0,0 +1,11 @@
+import rclpy
+from rclpy.node import Node
+
+
+
+class PerceptionNavNode(Node):
+    def __init__(self):
+        super().__init__('perception_nav_node')
+
+    def main(self):
+        pass
