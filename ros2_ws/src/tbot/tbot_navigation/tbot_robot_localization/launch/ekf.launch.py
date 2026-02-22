1d6c4e8e3a561fbd27f1b2bc52249bb4c57773a7
zeki-aitech <trungnh.aitech@gmail.com>
2026-02-22 23:34:06 +0000

    Fix parameter path in ekf.launch.py: change 'params' directory to 'param' for correct EKF configuration loading.

--- ros2_ws/src/tbot/tbot_navigation/tbot_robot_localization/launch/ekf.launch.py
+++ ros2_ws/src/tbot/tbot_navigation/tbot_robot_localization/launch/ekf.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
             name='ekf_filter_node',
             output='screen',
             parameters=[
-                os.path.join(get_package_share_directory("tbot_robot_localization"), 'params', 'ekf.yaml'),
+                os.path.join(get_package_share_directory("tbot_robot_localization"), 'param', 'ekf.yaml'),
                 {"use_sim_time": use_sim_time},
             ],
         ),
